5a0fdf423a94260f7cabef2b86d63c8219b48130
webyzf0109@163.com <webyzf0109@163.com>
2020-07-07 21:24:45 +0800

    yzf

--- main.js
+++ main.js
@@ -30,7 +30,6 @@ app.on('ready', () => {
     //在主线程下，通过ipcMain对象监听渲染线程传过来的saveDialog事件
     ipcMain.on('saveDialog', (event, arg) => {
         // 打开弹窗
-        console.log(arg.fileType,'arg.fileType')
         dialog.showSaveDialog(mainWindow, {
             // 在 Windows 和 Linux 上, 打开对话框不能同时是文件选择器和目录选择器, 因此如果在这些平台上将 properties 设置为["openFile"、"openDirectory"], 则将显示为目录选择器。
             properties: ['openFile', 'openDirectory'],
@@ -41,38 +40,35 @@ app.on('ready', () => {
                 ...extensionType[arg.fileType]
             ],
             // 点击保存回调
-        }, filePath => {
+        }).then(filePath => {
+
             // filePath存在则为保存路径 否为undefined
             // 去掉头部无用字段并将base64转码成buffer
-            console.log(222)
             let dataBuffer = Buffer.from(arg.baseCode.split('base64,')[1], 'base64')
             // 检测文件扩展名是否正确
             let typeFlag = extensionType[arg.fileType].some(item => {
-                if (filePath) {
-                    return item.extensions[0] === filePath.substring(filePath.lastIndexOf('.') + 1)
+                if (filePath&&filePath.filePath) {
+                    return item.extensions[0] === filePath.filePath.substring(filePath.filePath.lastIndexOf('.') + 1)
                 } else {
                     return false
                 }
             })
             if (typeFlag) {
-                fs.writeFile(filePath, dataBuffer, err => {
+                fs.writeFile(filePath.filePath, dataBuffer, err => {
                     // 失败
                     if (err) {
-                        console.log('err')
                         // 向渲染进程发送消息通知失败
                         mainWindow.webContents.send('defeatedDialog')
                     }
                 })
-                console.log('success')
                 // 成功 向渲染进程发送消息通知成功
                 mainWindow.webContents.send('succeedDialog')
                 // 判断是否存在保存路径
-            } else if (filePath !== undefined) {
-                console.log('cunzai')
+            } else if (filePath.filePath !== undefined) {
                 dialog.showMessageBox({
                     type: 'error',
                     title: '系统提示',
-                    message: '系统检测出文件类型异常，请检查并重新选择或填写'
+                    message: '取消操作'
                 })
             }
         })
--- src/App.js
+++ src/App.js
@@ -1,44 +1,19 @@
-import React, { useState } from 'react';
-import { Row, Col, Button } from 'antd';
-import FileSearch from './components/FileSearch/FileSearch'
-import FileList from './components/FileList/FileList'
-import TabList from './components/TabList/TabList'
+import React from 'react';
+import { Row, Button } from 'antd';
 import './App.scss';
 import './assets/styles/common.scss';
-import defaultFiles from './utils/defaultFiles'
-import { FolderAddOutlined, ImportOutlined } from '@ant-design/icons';
-import SimpleMDE from "react-simplemde-editor";
-import "easymde/dist/easymde.min.css";
 import axios from "axios"
 const { ipcRenderer } = window.require('electron')
 
 
 const App = () => {
-  const [files, setFiles] = useState(defaultFiles)
-  const [activeFileID, setActiveFileID] = useState(null)
-  const [openedFileIDs, setOpenedFileIDs] = useState([])
-  const [unsavedFileIDs, setUnsavedFileIDs] = useState([])
-  const activeFile = files[activeFileID]
-  const openedFiles = openedFileIDs.map(openID => {
-    return files[openID]
-  })
-  const handleChange = (value) => {
-    console.log(value, 'value')
-  }
-  const fileClick = (fileId) => {
-    setActiveFileID(fileId);
-    if (!openedFileIDs.includes(fileId)) {
-      setOpenedFileIDs([...openedFileIDs, fileId])
-    }
-  }
   const exportExcel = () => {
-    axios.get('/crawler/disabled/export').then(res => {
+    axios.get('/crawler/disabled/export',{responseType:'blob'}).then(res => {
       console.log(res)
-      let excelModel = new Blob([res.data], { type: "application/octet-stream" })
       // 创建一个FileReader的实例
       let reader = new FileReader()
       // 开始读取指定的Blob中的内容。一旦完成，result属性中将包含一个data: URL格式的Base64字符串以表示所读取文件的内容。
-      reader.readAsDataURL(excelModel)
+      reader.readAsDataURL(res.data)
       // 处理 load 事件。该事件在读取操作完成时触发
       reader.addEventListener("loadend", function () {
         console.log(reader.result)
@@ -46,63 +21,35 @@ const App = () => {
         // console.log(dataBuffer,'dataBuffer')
         // reader.result 包含被转化为类型数组 typed array 的 blob
         // 向主进程发送下载excel消息
+        let fileName=res.headers['content-disposition'];
+        console.log(fileName,'fileName')
         ipcRenderer.send("saveDialog", {
           baseCode: reader.result,
           fileType: 'excel',
-          fileName: '封神榜'
+          fileName: '残疾人基础信息'+fileName.substring(fileName.lastIndexOf('.')-8,fileName.lastIndexOf('.'))
         })
         // // 接收主进程发送回来的下载成功回调
         ipcRenderer.once('succeedDialog', event => {
+          alert('导出成功');
           // 成功回调
         })
         // // 接收主进程发送回来的下载失败回调
         ipcRenderer.once('defeatedDialog', event => {
           // 失败回调
+          alert('导出出错');
         })
       })
     })
   }
   return (
     <div className="App">
-      <Row>
-        <Col className="left-box" span={6}>
-          <FileSearch title="To do List" onFileSearch={(value) => { console.log(value, 'value') }}></FileSearch>
-          <FileList
-            files={files}
-            onFileClick={fileClick}
-            onFileDelete={(id) => { console.log(id, 'file.id delete') }}
-            onSaveEdit={() => { }}>
-          </FileList>
-          <div className="bottomBox">
-            <Button icon={<FolderAddOutlined />} onClick={() => { console.log('add') }} type="primary">
-              新建
-            </Button>
-            <Button icon={<ImportOutlined />} onClick={exportExcel} type="primary" >
-              导出
-            </Button>
-          </div>
-        </Col>
-        <Col className="right-box" span={18}>
-          {!activeFile &&
-            <div className="start-page">
-              选择或者创建新的 Markdown 文档
-            </div>
-          }
-          {
-            activeFile &&
-            <>
-              <TabList
-                files={openedFiles}
-                unsaveIds={unsavedFileIDs}
-                onTabClcik={(id) => { setActiveFileID(id); console.log(2) }}
-                onCloseTab={() => { console.log(111) }}
-                activeId={activeFileID}></TabList>
-              <SimpleMDE value={activeFile.body} options={{ minHeight: '450px' }} onChange={(value) => { handleChange(value) }} />;
-            </>
-          }
-
-        </Col>
-
+      <header className="App-header">
+          点击下方按钮进行数据导出
+      </header>
+      <Row> 
+          <Button className="button" onClick={exportExcel} type="primary" >
+            导出
+          </Button>
       </Row>
     </div>
   );
